1ac9b489979c6ec73756ea3c0d2fa9311d167659
Marcelo Morais <marcelodotmorais@gmail.com>
2019-06-16 21:15:26 -0300

    Commit inicial

---
+++ spaceInvaders.vhd
@@ -0,0 +1,18 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+
+entity spaceInvaders is
+  port (
+    clock   : in std_logic;
+    rst     : in std_logic
+  );
+end spaceInvaders;
+
+architecture aSpaceInvaders of spaceInvaders is
+  
+begin
+  
+  
+  
+end architecture aSpaceInvaders;
